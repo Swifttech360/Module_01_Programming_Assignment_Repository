2e8d495b432b2ae35d84ff3a828b47af8b632084
Swifttech360 <swifttech360@gmail.com>
2025-01-21 20:27:59 -0500

    Program formats the infpu and output using logical boolean

--- main.py
+++ main.py
@@ -7,14 +7,22 @@
 # Fahrenheit. (15 points)
 while True:
     try:
-        celsius = float(input('Enter Celsius temp to convert to Fahrenheit: \n'))
+        unformattedC = float(input('Enter Celsius temp to convert to Fahrenheit: \n'))
     except ValueError:
         print('Invalid input \nPlease try again.\n')
         continue
-    unformattedF = (9/5 * celsius + 32)
-    
-    fahrenheit = (f"{unformattedF:.0f}" if unformattedF % 1 == 0
-        else f"{unformattedF:.1f}" if round(unformattedF % 1, 1) == unformattedF % 1
-        else f"{unformattedF:.2f}"
-    )
-    print(f'\n{celsius}ºC is equal to {fahrenheit}ºF')
+    unformattedF = (unformattedC)
+        #(9/5 * celsius + 32)
+
+
+    fahrenheit = (
+f"{unformattedF:.0f}" if unformattedF % 1 == 0
+ else f"{unformattedF:.1f}" if round(unformattedF % 1, 1) == unformattedF  % 1
+  else f"{unformattedF:.0f}" if round(unformattedF % 1, 4) == unformattedF % 1
+   else f"{unformattedF:.2f}")
+
+    celsius = f"{unformattedC:.0f}" if unformattedC % 1 == 0 else unformattedC
+
+
+
+    print(f'\n{unformattedC:.2f}ºC is equal to {fahrenheit}ºF')
